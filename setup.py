b917489f5e0aed6590b022e957a15ed3c25c9ab0
Peter Kuma <peter@peterkuma.net>
2023-09-07 15:54:36 +0100

    setup.py: Fix packages argument

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name='pst-format',
 	version='2.0.0',
-	packages=find_packages(),
+	packages=['pst', 'pst.bin'],
 	entry_points={
 		'console_scripts': [
 			'pst = pst.bin.pst:main',
